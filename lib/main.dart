e1b711765d4a77f75344caa0d7b6776f6b0dd3e2
HigorSouza <higor.andrade@hotmail.com>
2023-07-30 15:18:30 -0300

    Injeção de dependência com GetX

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,12 @@ import 'package:get/get.dart';
 import 'package:getx_intro/user_controller.dart';
 
 void main() {
+  // Injeção de dependência imediata
+  // Get.put<UserController>(UserController());
+
+  // Injeção de dependência tardia
+  Get.lazyPut<UserController>(() => UserController());
+
   runApp(const MyApp());
 }
 
@@ -27,12 +33,7 @@ class HomePage extends StatelessWidget {
   final nameController = TextEditingController();
   final ageController = TextEditingController();
 
-  TextStyle commonStyle() => const TextStyle(
-        fontSize: 17,
-        fontWeight: FontWeight.w500,
-      );
-
-  final userController = UserController();
+  final UserController userController = Get.find();
 
   @override
   Widget build(BuildContext context) {
@@ -43,28 +44,6 @@ class HomePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            // Apresentação do nome
-            Obx(
-              () => Text(
-                'Nome: ${userController.user.value.name}',
-                style: commonStyle(),
-              ),
-            ),
-
-            // Apresentação da idade
-            Obx(
-              () => Text(
-                'idade: ${userController.user.value.age}',
-                style: commonStyle(),
-              ),
-            ),
-
-            const Divider(
-              thickness: 1.5,
-              color: Colors.blue,
-              height: 20,
-            ),
-
             Row(
               crossAxisAlignment: CrossAxisAlignment.end,
               children: [
@@ -116,6 +95,63 @@ class HomePage extends StatelessWidget {
 
             // Espaçamento
             const SizedBox(height: 10),
+
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return DataScreen();
+                    },
+                  ),
+                );
+              },
+              child: const Text('Tela de dados'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DataScreen extends StatelessWidget {
+  DataScreen({
+    Key? key,
+  }) : super(key: key);
+
+  TextStyle commonStyle() => const TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.w700,
+      );
+
+  final UserController controller = Get.find();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dados'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            // Apresentação do nome
+            Obx(
+              () => Text(
+                'Nome: ${controller.user.value.name}',
+                style: commonStyle(),
+              ),
+            ),
+
+            // Apresentação da idade
+            Obx(
+              () => Text(
+                'Idade: ${controller.user.value.age}',
+                style: commonStyle(),
+              ),
+            ),
           ],
         ),
       ),
