fae76024f5c054eb20ffa31fbb93df830cef7742
HigorSouza <higor.andrade@hotmail.com>
2023-07-30 13:54:11 -0300

    Adicionando os primeiros arquivos do projeto.

---
+++ lib/main.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  final nameController = TextEditingController();
+  final ageController = TextEditingController();
+
+  TextStyle commonStyle() => const TextStyle(
+        fontSize: 17,
+        fontWeight: FontWeight.w500,
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'Nome: ',
+              style: commonStyle(),
+            ),
+            Text(
+              'idade: ',
+              style: commonStyle(),
+            ),
+
+            const Divider(
+              thickness: 1.5,
+              color: Colors.blue,
+              height: 20,
+            ),
+
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                // Campo de nome
+                Expanded(
+                  child: TextField(
+                    controller: nameController,
+                    decoration: const InputDecoration(
+                      labelText: 'Nome',
+                    ),
+                  ),
+                ),
+
+                // Botão para salvar o nome
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Salvar'),
+                ),
+              ],
+            ),
+
+            // Espaçamento
+            const SizedBox(height: 10),
+
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                // Campo de idade
+                Expanded(
+                  child: TextField(
+                    controller: ageController,
+                    decoration: const InputDecoration(
+                      labelText: 'Idade',
+                    ),
+                  ),
+                ),
+
+                // Botão para salvar a idade
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Salvar'),
+                ),
+              ],
+            ),
+
+            // Espaçamento
+            const SizedBox(height: 10),
+          ],
+        ),
+      ),
+    );
+  }
+}
